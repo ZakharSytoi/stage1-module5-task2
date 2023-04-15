40fd78143f227a59396e7cfc6e9162145d6bc06f
ZakharSytoi <zakharsytoi@gmail.com>
2023-04-15 11:54:07 +0200

    Task completed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
+    @FunctionalInterface
+    public interface Operation<T> {
+        List<T> apply(List<T> arg);
+    }
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list ->
+                list.stream().map(element ->
+                        Integer.divideUnsigned(element, divider)).collect(Collectors.toList());
     }
+
+
 }
